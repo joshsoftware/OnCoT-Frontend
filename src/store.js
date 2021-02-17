f6c1c68b5e93874244516b7e2b449c04fd1f2653
vaibhavchalse99 <vaibhav.chalse99@gmail.com>
2021-02-17 19:17:37 +0530

    Added and configured bootstrap, redux, saga

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import rootReducer from "reducers/rootReducer";
+import rootSagas from "sagas/rootSagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(rootSagas);

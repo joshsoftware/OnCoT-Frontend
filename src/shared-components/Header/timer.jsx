f64756e585e0f211c30cf2178dded638fea2dd31
Atharva Borekar <athessi10@gmail.com>
2021-02-23 16:44:01 +0530

    Timer gets value from API

--- src/shared-components/Header/timer.jsx
+++ src/shared-components/Header/timer.jsx
@@ -1,6 +1,13 @@
-localStorage.setItem("counter", 3600);
-
-const time = () => {  
+const axios = require("axios").default;
+const time = async () => {  
+    await axios("https://www.random.org/integers/?num=1&min=1800&max=7200&col=1&base=10&format=plain&rnd=new")
+        .then((response) => {
+            localStorage.setItem("counter", response.data);
+        }).catch((error) => {
+            console.log(error);
+    })
+    
+    console.log(localStorage.getItem("counter"));
     localStorage.getItem("counter") > 0 && setInterval(() => {
         let counter = localStorage.getItem("counter");
         localStorage.setItem("counter", counter-1);

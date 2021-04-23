aaac62224d073392d699e4ebe9a0aa01fedbf5cd
Atharva Borekar <athessi10@gmail.com>
2021-04-23 17:02:12 +0530

    Codacy Issues fix 1

--- src/shared-components/Header/Webcam/WebcamCapture/index.jsx
+++ src/shared-components/Header/Webcam/WebcamCapture/index.jsx
@@ -14,7 +14,6 @@ const WebcamCapture = () => {
     const interval = setInterval(() => {
       if (webcamRef.current != null) {
         const imageSrc = webcamRef.current.getScreenshot();
-        console.log(imageSrc);
       }
     }, 3000);
     return () => clearInterval(interval);

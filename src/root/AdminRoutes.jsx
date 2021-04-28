cde34fb9b3cd55709e4db7040ac79c92789da6ec
Atharva Borekar <athessi10@gmail.com>
2021-04-28 15:01:48 +0530

    Admin side redirect to login page when user is unauthorised

--- src/root/AdminRoutes.jsx
+++ src/root/AdminRoutes.jsx
@@ -1,17 +1,39 @@
-import { Redirect, Route, Switch, useRouteMatch } from 'react-router-dom';
+import {
+  Redirect,
+  Route,
+  Switch,
+  useHistory,
+  useRouteMatch,
+} from 'react-router-dom';
 
 import ProtectedAdminRoute from 'HOC/ProtectedAdminRoute';
 import LoginContainer from 'modules/admin/login/LoginContainer';
 import HomeContainer from 'modules/admin/home/HomeContainer';
 
-import { ADMIN_ROUTES, ROUTES } from 'constants/routeConstants';
-import CreateProblemContainer from 'modules/admin/createProblem/CreateProblemContainer';
-import ShowCandidateListContainer from 'modules/admin/showCandidateList/ShowCandidateListContainer';
-import SendEmailInviteContainer from 'modules/admin/sendEmailInvite/SendEmailInviteContainer';
-import DriveResultContainer from 'modules/admin/driveResult/DriveResultContainer';
+import { ADMIN_ROUTES } from 'constants/routeConstants';
+import { ADMIN_LOGIN } from 'redux/admin/login/actionConstants';
+
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 function AdminRoutes() {
   const { path } = useRouteMatch();
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+  axios.interceptors.response.use(
+    (res) => res,
+    (err) => {
+      if (err.response.status === 401) {
+        dispatch({
+          type: ADMIN_LOGIN.ADMIN_FAILURE_ACTION,
+          payload: 'Unauthorized User',
+        });
+        history.push('/admin/login');
+      }
+      throw err;
+    },
+  );
   return (
     <Switch>
       <Route
@@ -19,11 +41,7 @@ function AdminRoutes() {
         path={path + ADMIN_ROUTES.LOGIN}
         component={LoginContainer}
       />
-      <Route
-        exact
-        path={path}
-        component={LoginContainer}
-      />
+      <Route exact path={path} component={LoginContainer} />
       <ProtectedAdminRoute
         path={path + ADMIN_ROUTES.HOME}
         component={HomeContainer}

e6e7844defb2171bd645e2ff8005efed2f03faf0
onkar-josh <onkar.hasabe@joshsoftware.com>
2021-07-14 10:27:31 +0530

    resolved test case failuer issue

--- src/sagas/__tests__/codeSubmissionSaga.test.js
+++ src/sagas/__tests__/codeSubmissionSaga.test.js
@@ -1,12 +1,13 @@
 import { call, put } from 'redux-saga/effects';
 
-import { codeSubmissionSaga } from 'sagas/codeSubmissionSaga';
+import { codeSubmissionSaga, codeSubmissionStatusSaga } from 'sagas/codeSubmissionSaga';
 import {
   submitRequest,
   submitAction,
   submitRequestFailed,
 } from 'actions/codeSubmissionActions';
-import codeSubmissionPostApi from 'apis/codeSubmissionApi';
+import { codeSubmissionPostApi } from 'apis/codeSubmissionApi';
+import local from 'utils/local';
 
 describe('CodeSubmission Saga', () => {
   let gen;
@@ -16,33 +17,51 @@ describe('CodeSubmission Saga', () => {
         submissionAllowed: 2,
         testcasesPassed: 3,
         totalTestcases: 5,
+        submission_id: 1,
+        status: 'accepted',
       },
     },
   };
   const errorMessage = 'Error Message 404';
   const action = submitRequest({
     code: 'cout<<"hello"<<endl;↵cin>>n;',
-    id: 1,
+    problemId: 1,
+    languageId: 76,
     languageSelected: { id: '76', name: 'C++ (Clang 7.0.1)' },
-    submissionCount: 2,
+    submissionAllowed: 2,
+    candidateId: 1,
+    driveID: 1,
+    token: 'abc',
   });
+
+  local.setItem('authToken', 'abc');
 
   beforeEach(() => {
     gen = codeSubmissionSaga(action);
   });
 
   it('API call should be successful', () => {
-    expect(gen.next(action.payload).value).toEqual(call(codeSubmissionPostApi, action.payload));
+    const data = {
+      source_code:action.payload.code,
+      language_id:action.payload.languageId,
+      id: action.payload.problemId,
+      submission_count:action.payload.submissionAllowed,
+      candidate_id:parseInt(action.payload.candidateId, 10),
+      drive_id:parseInt(action.payload.driveID, 10),
+      token: action.payload.token,
+    };
+    expect(gen.next(action.payload).value).toEqual(call(codeSubmissionPostApi, data));
   });
 
   it('Dispactch success action', () => {
     gen.next();
-    expect(gen.next(response).value).toEqual(put(submitAction(response.data)));
+    expect(gen.next(response).value).toEqual(call(codeSubmissionStatusSaga, { submission_id: 1,
+      status: 'accepted' }));
     expect(gen.next().done).toEqual(true);
   });
 
   it('Dispatch failure action', () => {
     gen.next();
-    expect(gen.throw(errorMessage).value).toEqual(put(submitRequestFailed()));
+    expect(gen.throw(errorMessage).value).toEqual(put(submitRequestFailed("Submission Failed, Please try again!")));
   });
 });

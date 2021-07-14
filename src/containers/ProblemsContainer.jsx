78c68d3559d8e5bca5ab7c33d4ed0600e7937325
onkar-josh <onkar.hasabe@joshsoftware.com>
2021-07-14 10:12:36 +0530

    added changes in problems container regarding multiple problems

--- src/containers/ProblemsContainer.jsx
+++ src/containers/ProblemsContainer.jsx
@@ -7,19 +7,25 @@ import { statementRequest } from 'actions/problemStatementActions';
 const ProblemsContainer = () => {
   const dispatch = useDispatch();
   const result = useSelector((state) => state.problemStatementReducer);
+
   const {
     data: { id },
   } = useSelector((state) => state.userDriveReducer);
+
   const {
-    statement: { title, description },
+    activeIndex,
     errorMessage,
     isError,
     isLoading,
+    statement,
   } = result;
+
+  const { title = ' ', description = ' ' }  = statement[activeIndex - 1] || {};
 
   useEffect(() => {
     dispatch(statementRequest(id));
   }, [dispatch]);
+
   return (
     <ProblemsComponent
       isError={isError}

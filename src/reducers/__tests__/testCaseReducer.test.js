20297a8b9507a2f5043f1e43333d7dc39eb21315
onkar-josh <onkar.hasabe@joshsoftware.com>
2021-07-15 09:22:58 +0530

    changed test case reducer

--- src/reducers/__tests__/testCaseReducer.test.js
+++ src/reducers/__tests__/testCaseReducer.test.js
@@ -1,21 +1,21 @@
-import testCaseReducer, {initialState} from '../../redux/admin/testCase/reducer';
-import { createTestCaseSuccessAction, createTestCaseFailureAction } from '../../redux/admin/testCase/action';
+import createTestCaseReducer, { initialState } from 'redux/admin/testCase/reducer';
+import { createTestCaseSuccessAction, createTestCaseFailureAction } from 'redux/admin/testCase/action';
 
 describe('TestCase reducer', () => {
-    it('return default state', () => {
-         expect(testCaseReducer(initialState,{})).toEqual(initialState);
-    });
+  it('return default state', () => {
+    expect(createTestCaseReducer(initialState, {})).toEqual(initialState);
+  });
 
-    it('check success request', () => {
-        const testCaseMessage = 'success';
-        const isTestCaseSuccess = true;     
-         expect(testCaseReducer(initialState,createTestCaseSuccessAction({message: 'success'}, isTestCaseSuccess))).toEqual({...initialState, testCaseMessage, isTestCaseSuccess});
-    });
+  it('check success request', () => {
+    const testCaseMessage = 'success';
+    const isTestCaseSuccess = true;
+    expect(createTestCaseReducer(initialState, createTestCaseSuccessAction({ message: 'success' }, isTestCaseSuccess))).toEqual({ ...initialState, testCaseMessage, isTestCaseSuccess });
+  });
 
-    it('Request Failed', () => {
-        const testCaseErrorMessage = '401 Request failed!';
-        const isTestCaseError = true;
-        expect(testCaseReducer(initialState, createTestCaseFailureAction({error: '401 Request failed!'}, isTestCaseError)))
-          .toEqual({ ...initialState, testCaseErrorMessage, isTestCaseError });
-      });
+  it('Request Failed', () => {
+    const testCaseErrorMessage = '401 Request failed!';
+    const isTestCaseError = true;
+    expect(createTestCaseReducer(initialState, createTestCaseFailureAction({ error: '401 Request failed!' }, isTestCaseError)))
+      .toEqual({ ...initialState, testCaseErrorMessage, isTestCaseError });
+  });
 });
